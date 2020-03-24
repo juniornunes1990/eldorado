f0b9f3f50eb5b32e4763a33eff6b9bf20005455c
Junior Nunes <juniornunesdv@gmail.com>
2020-03-23 23:55:17 -0300

    Auto load Atlantic OK! classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,14 +6,55 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitee0b4155909b495fa3ea382f8ea945fd
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'A' => 
+        array (
+            'Atlantic\\' => 9,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Atlantic\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/atlantic/php-classes/src',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'S' => 
+        array (
+            'Slim' => 
+            array (
+                0 => __DIR__ . '/..' . '/slim/slim',
+            ),
+        ),
+        'R' => 
+        array (
+            'Rain' => 
+            array (
+                0 => __DIR__ . '/..' . '/rain/raintpl/library',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitee0b4155909b495fa3ea382f8ea945fd::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitee0b4155909b495fa3ea382f8ea945fd::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitee0b4155909b495fa3ea382f8ea945fd::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitee0b4155909b495fa3ea382f8ea945fd::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitee0b4155909b495fa3ea382f8ea945fd::$classMap;
 
         }, null, ClassLoader::class);
     }
