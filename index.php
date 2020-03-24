79739bebb648ad97c092872c43a69c9067d37062
Junior Nunes <juniornunesdv@gmail.com>
2020-03-24 01:47:02 -0300

    Auto load Atlantic OK! classe

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Atlantic\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
-    
-	$sql = new Atlantic\DB\Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);	
+	$page = new Page();
+	
+	$page->setTpl("index");
 
 });
 
